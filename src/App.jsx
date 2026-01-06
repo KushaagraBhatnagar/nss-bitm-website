5628966cee2f870c6881c35f500012663665c7ef
CallMeChandler <aakarsh0324@gmail.com>
2026-01-06 16:10:18 +0530

    Initialize Vite React app with Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="p-10">
+      {/* Title */}
+      <div className="text-4xl font-bold mb-8">NSS BITM</div>
+
+      {/* Logos */}
+      <div className="flex gap-6 mb-6">
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      {/* Content */}
+      <h1 className="text-2xl mb-4">Vite + React</h1>
+
+      <div className="card mb-4">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
